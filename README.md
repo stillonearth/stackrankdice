662b9d8c734c0db31e94ee57ca4a5d2d95774d2d
Sergei Surovtsev <97428129+stillonearth@users.noreply.github.com>
2022-10-12 05:25:25 +0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# stackrankdice
-StackrankDice the game built with Bevy
+# StackrankDice
+
+This is re-implementation of [DiceWars](https://www.gamedesign.jp/games/dicewars/) game by [GAMEDESIGN.jp](https://www.gamedesign.jp/)
+
+THIS IS WORK IN PROGRESS
+
+![image](https://www.webpagescreenshot.info/image-url/HcuZ-mQaC)
+
+## How to play
+
+On a hexagon board each player starts with a number of regions. Each region has a number of dice. The goal is to conquer all regions of the opponent.
+
+Battle mechanics is simple: each player rolls a number of dice equal to the number of dice in the region. The player with the highest number of dice wins. In case of a tie, the attacker loses.
+
+Conquered regions are added to the attacker's stack. The attacker can choose to move some of the dice to the conquered region. The number of dice in the conquered region cannot be less than 1.
+
+## Implementation
+
+This is a re-implementation with [Bevy](https://bevyengine.org/) engine on Rust langaage. The original game was written in Flash.
+
+## Acknowledgements
+
+- [bevy-hex-example](https://github.com/Quantumplation/bevy-hex-example) by [Pi Lanningham](https://github.com/Quantumplation/bevy-hex-example) — for general hex grid implementation. Code has no license on github.
--- src/main.rs
+++ src/main.rs
@@ -1,177 +1,19 @@
-use std::collections::HashMap;
-
-use rand::seq::IteratorRandom;
-use rand::Rng;
+mod game;
+mod geometry;
+mod hex;
 
 use bevy::{
     prelude::*,
     render::{mesh::Indices, render_resource::PrimitiveTopology},
 };
-use bevy_hex::{hex::HexCoord, *};
 use bevy_inspector_egui::WorldInspectorPlugin;
 use bevy_mod_outline::*;
+use bevy_mod_picking::*;
 
-const BOARD_SIZE: isize = 16;
-const NUMBER_OF_PLAYERS: usize = 2;
-const NUMBER_OF_PATCHES: usize = 16;
+use rand::Rng;
 
-#[derive(Default)]
-struct Board {
-    hexes: HashMap<(isize, isize), usize>,
-    regions: Vec<Region>,
-}
-
-#[derive(Default)]
-struct Region {
-    hexes: Vec<(isize, isize)>,
-    #[allow(dead_code)]
-    owner: usize,
-}
-
-impl Region {
-    pub fn center_of_mass(&self) -> (f32, f32) {
-        let mut x = 0.0;
-        let mut y = 0.0;
-        for (hx, hy) in self.hexes.iter() {
-            x += *hx as f32;
-            y += *hy as f32;
-        }
-
-        (x / self.hexes.len() as f32, y / self.hexes.len() as f32)
-    }
-
-    pub fn center_hex(&self) -> HexCoord {
-        let center = self.center_of_mass();
-        let mut nearest_hex: (isize, isize) = (0, 0);
-        let mut min_distance = f32::MAX;
-        for point in self.hexes.iter() {
-            let distance =
-                ((center.0 - point.0 as f32).powi(2) + (center.1 - point.1 as f32).powi(2)).sqrt();
-
-            if distance < min_distance {
-                min_distance = distance;
-                nearest_hex = *point;
-            }
-        }
-
-        HexCoord::new(nearest_hex.0, nearest_hex.1)
-    }
-}
-
-fn generate_board() -> Board {
-    const HALF_BOARD_SIZE: isize = BOARD_SIZE / 2 - 1;
-    // Roughly half of the board occupied by patches (squads)
-    const PATCH_SIZE: isize =
-        (BOARD_SIZE * BOARD_SIZE) / (NUMBER_OF_PATCHES * NUMBER_OF_PLAYERS * 2) as isize;
-
-    let mut board = Board::default();
-    let mut rng = rand::thread_rng();
-
-    for patch in 0..NUMBER_OF_PATCHES {
-        for player in 0..NUMBER_OF_PLAYERS {
-            let mut is_starting_point_valid = false;
-
-            while !is_starting_point_valid {
-                let mut has_neighbours = false;
-
-                while !has_neighbours {
-                    let mut hex_snapshot = board.hexes.clone();
-
-                    let q = rng.gen_range(-HALF_BOARD_SIZE..HALF_BOARD_SIZE);
-                    let r = rng.gen_range(-HALF_BOARD_SIZE..HALF_BOARD_SIZE);
-
-                    // check if starting position is empty
-                    let initial_coord = (q, r);
-                    if board.hexes.get(&initial_coord).is_none() {
-                        is_starting_point_valid = true;
-                        hex_snapshot.insert(initial_coord, player);
-                    } else {
-                        // try over
-                        continue;
-                    }
-
-                    // expand until size limit is reached or no more space to grow
-                    let mut patch_hexes: Vec<(isize, isize)> = vec![initial_coord];
-
-                    for _ in 0..PATCH_SIZE {
-                        // find a bordering hex. use random iterating order to avoid bias
-                        let mut border_hex: Option<HexCoord> = None;
-                        for coord in patch_hexes
-                            .iter()
-                            .choose_multiple(&mut rng, patch_hexes.iter().len())
-                        {
-                            let hex = HexCoord::new(coord.0, coord.1);
-                            // iterate over all neighbors and find a free one
-                            for neighbor in hex.neighbors() {
-                                let neighbour_coord = (neighbor.q, neighbor.r);
-                                if hex_snapshot.get(&neighbour_coord).is_none() {
-                                    border_hex = Some(hex.clone());
-                                    break;
-                                }
-                            }
-
-                            // continue expanding a border hex
-                            if border_hex.is_some() {
-                                break;
-                            }
-                        }
-
-                        // no more hex cells in this patch
-                        if border_hex.is_none() {
-                            break;
-                        }
-
-                        // add a new hex to the patch
-                        let mut candidates: Vec<(isize, isize)> = vec![];
-                        for neighbor in border_hex.unwrap().neighbors() {
-                            let neighbour_coord = (neighbor.q, neighbor.r);
-                            if board.hexes.get(&neighbour_coord).is_none() {
-                                candidates.push(neighbour_coord);
-                            }
-                        }
-                        let candidate = candidates.iter().choose(&mut rng).unwrap();
-                        patch_hexes.push(*candidate);
-                        hex_snapshot.insert(*candidate, player);
-                    }
-
-                    if patch_hexes.len() == 1 {
-                        break;
-                    }
-
-                    // check whether patch has any neighbors or start over
-                    for patch_hex in patch_hexes.iter() {
-                        let hex = HexCoord::new(patch_hex.0, patch_hex.1);
-                        for neighbor in hex.neighbors() {
-                            if board.hexes.get(&(neighbor.q, neighbor.r)).is_some() {
-                                has_neighbours = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    // if could not generate a patch with a neightbours, start over
-                    // except for the first patch
-                    if player == 0 && patch == 0 {
-                        has_neighbours = true;
-                    }
-
-                    // if patch has neighbours, add it to the board
-                    // else, start over
-                    if has_neighbours {
-                        board.hexes = hex_snapshot;
-                        board.regions.push(Region {
-                            hexes: patch_hexes,
-                            owner: player,
-                        });
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    board
-}
+use crate::geometry::flat_hexagon_points;
+use crate::hex::HexCoord;
 
 /// Generate a single hex mesh
 fn generate_hex_region_mesh(hexes: Vec<(isize, isize)>) -> Mesh {
@@ -181,11 +23,11 @@ fn generate_hex_region_mesh(hexes: Vec<(isize, isize)>) -> Mesh {
     let mut indices: Vec<u32> = vec![];
 
     for (hex_num, hex) in hexes.iter().enumerate() {
-        let c = hex::HexCoord::new(hex.0, hex.1);
+        let c = HexCoord::new(hex.0, hex.1);
         let hex_num = hex_num as u32;
 
         // Populate the points for the top face, as a slightly scaled hexagon
-        geometry::flat_hexagon_points(&mut pts, 1.0, &c);
+        flat_hexagon_points(&mut pts, 1.0, &c);
         for _ in 0..9 {
             normals.push([0., 1., 0.]);
         }
@@ -196,8 +38,6 @@ fn generate_hex_region_mesh(hexes: Vec<(isize, isize)>) -> Mesh {
             indices.push(18 * hex_num + i + 2); // Next point  North-east     North-west
         }
 
-        println!("pts: {:?}", pts.len());
-
         // Duplicate points with an offset as a bottom face
         for p in pts.len() - 9..pts.len() {
             pts.push([pts[p][0], pts[p][1] - 0.005, pts[p][2]]);
@@ -205,8 +45,6 @@ fn generate_hex_region_mesh(hexes: Vec<(isize, isize)>) -> Mesh {
         for _ in 0..9 {
             normals.push([0., -1., 0.]);
         }
-
-        println!("pts: {:?}", pts.len());
 
         // Populate indices for bottom
         for i in 0..=6 {
@@ -252,10 +90,11 @@ pub fn setup(
     commands
         // camera
         .spawn_bundle(Camera3dBundle {
-            transform: Transform::from_translation(Vec3::new(40.0, 42., 0.0))
+            transform: Transform::from_translation(Vec3::new(50.0, 42., 0.0))
                 .looking_at(Vec3::default(), Vec3::Y),
             ..Default::default()
-        });
+        })
+        .insert_bundle(PickingCameraBundle::default());
 
     // Lightning
     const HALF_SIZE: f32 = 10.0;
@@ -272,7 +111,7 @@ pub fn setup(
                 ..default()
             },
             illuminance: 10000.0,
-            shadows_enabled: false,
+            shadows_enabled: true,
             ..default()
         },
         transform: Transform {
@@ -284,7 +123,7 @@ pub fn setup(
     });
 
     let colors = [Color::PURPLE, Color::CYAN];
-    let board = generate_board();
+    let board = game::generate_board();
 
     // Draw board
     let mut rng = rand::thread_rng();
@@ -313,9 +152,8 @@ pub fn setup(
                     width: 1.0,
                 },
                 ..default()
-            });
-
-        // return;
+            })
+            .insert_bundle(PickableBundle::default());
     }
 
     // Place dice on areas
@@ -340,6 +178,7 @@ fn main() {
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPickingPlugins)
         .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(OutlinePlugin)
         .add_startup_system(setup)
