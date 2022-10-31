13eaa1ce4de23fbc0163773abcf9ded9b733a792
Sergei Surovtsev <97428129+stillonearth@users.noreply.github.com>
2022-10-31 17:38:11 +0300

    adjusted patch size

--- src/game.rs
+++ src/game.rs
@@ -7,7 +7,7 @@ use rand::Rng;
 use crate::hex::HexCoord;
 
 const BOARD_SIZE: isize = 20;
-const NUMBER_OF_PATCHES: usize = 3;
+const NUMBER_OF_PATCHES: usize = 16;
 
 #[derive(Default, Clone)]
 pub struct Board {
