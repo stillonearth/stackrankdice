ba0fbe1be93b04f6b245a3d517533d474eee9645
William King <quentusrex@gmail.com>
2022-11-08 10:20:58 -0800

    Remove println here, if there isn't a debug line used in main lib

--- tests/startup_determinism.rs
+++ tests/startup_determinism.rs
@@ -17,7 +17,6 @@ fn fixed_world_undef_env_seed() {
 
     let mut map_prng = app.world.get_resource_mut::<PrngMapResource>().unwrap();
 
-    println!("map_prng: {:?}", map_prng);
     let first: f32 = map_prng.rng.gen_range(0.0..=0.0001);
     assert_eq!(first, 2.1680013e-5);
 }
